f6f835a55f7e016209a1a22b80a82eb635c87cde
liaoyunxia <372021030@qq.com>
2019-01-03 19:57:07 +0800

    sd

--- lete/settings.py
+++ lete/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'lete.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'lete',
+    #     'USER': 'lete',
+    #     'PASSWORD': 'Liao0726',
+    #     'HOST': '127.0.0.1',
+    #     'POST': '3306',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lete',
-        'USER': 'lete',
-        'PASSWORD': 'Liao0726',
-        'HOST': '127.0.0.1',
-        'POST': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
