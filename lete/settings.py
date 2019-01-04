00cd8182017059ddd5848c215b35b1578f431fd5
liaoyunxia <372021030@qq.com>
2019-01-04 13:31:35 +0800

    sd

--- lete/settings.py
+++ lete/settings.py
@@ -53,8 +53,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'lete.urls'
-# ROOT_URLCONF = 'urls'
+# ROOT_URLCONF = 'lete.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
