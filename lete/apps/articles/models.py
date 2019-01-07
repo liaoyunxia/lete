d43987559baeba0bf277fcd7b5c32831667a61b6
liaoyunxia <372021030@qq.com>
2019-01-07 17:38:00 +0800

    sd

--- lete/apps/articles/models.py
+++ lete/apps/articles/models.py
@@ -17,3 +17,7 @@ class Article(UserModel, TimeModel, StateModel):
     import_mothon = models.IntegerField(_('import_method'), choices=ARTICLE_IMPORT_METHOD_CHOICES, default=0)
     name = models.CharField(_('name'), max_length=50)
     key_word = models.CharField(_('key_word'), max_length=150, blank=True, default='')
+
+    class Meta:
+        verbose_name = _('article')
+        verbose_name_plural = _('article')
