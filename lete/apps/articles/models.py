a66fbcd6ac69c6936a5ade999f8384a4b0806592
liaoyunxia <372021030@qq.com>
2019-01-07 18:05:36 +0800

    sd

--- lete/apps/articles/models.py
+++ lete/apps/articles/models.py
@@ -10,7 +10,7 @@ from django.utils.translation import ugettext_lazy as _
 from common.models import UserModel, TimeModel, StateModel, NoModel
 
 ARTICLE_IMPORT_METHOD_CHOICES = ((0, ' 用户编辑'), (1, '爬虫获取'), (2, '用户复制'))
-
+ARTICLE_TYPE_CHOICES = ((0, '草稿'), (1, '待审核'), (2, '过期'), (3, '显示'))
 
 class Article(UserModel, TimeModel, StateModel, NoModel):
     url = models.CharField(_('url'), max_length=100, blank=True)
@@ -18,6 +18,7 @@ class Article(UserModel, TimeModel, StateModel, NoModel):
     name = models.CharField(_('name'), max_length=50)
     content = models.TextField(_('content'), blank=True)
     remark = models.CharField(_('remark'), max_length=200, blank=True)
+    type = models.IntegerField(_('import_method'), choices=ARTICLE_TYPE_CHOICES, default=0)
 
     class Meta:
         verbose_name = _('article')
