0837c7e439636e69996f39752a5e0319e95f41e6
liaoyunxia <372021030@qq.com>
2019-01-08 10:40:05 +0800

    sd

--- lete/apps/articles/models.py
+++ lete/apps/articles/models.py
@@ -12,6 +12,7 @@ from common.models import UserModel, TimeModel, StateModel, NoModel
 ARTICLE_IMPORT_METHOD_CHOICES = ((0, ' 用户编辑'), (1, '爬虫获取'), (2, '用户复制'))
 ARTICLE_TYPE_CHOICES = ((0, '草稿'), (1, '待审核'), (2, '过期'), (3, '显示'))
 
+
 class Article(UserModel, TimeModel, StateModel, NoModel):
     url = models.CharField(_('url'), max_length=100, blank=True)
     import_method = models.IntegerField(_('import_method'), choices=ARTICLE_IMPORT_METHOD_CHOICES, default=0)
